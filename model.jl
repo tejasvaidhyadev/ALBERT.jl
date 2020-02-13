b05305da526dab19466f88883917da07869cb601
tejasvaidhyadev <iamtejasvaidhya@gmail.com>
2020-02-14 03:38:27 +0530

    adding ALBERT structure and model.jl

--- model.jl
+++ model.jl
@@ -1,34 +1,36 @@
 const config = Dict(
-  "attention_probs_dropout_prob" => 0.1,
   "initializer_range"            => 0.02,
   "hidden_act"                   => gelu,
+  "embedding"                    => 128,
   "num_hidden_layers"            => 12,
   "hidden_size"                  => 768,
   "max_position_embeddings"      => 512,
   "type_vocab_size"              => 2,
   "vocab_size"                   => 30522,
   "num_attention_heads"          => 12,
-  "hidden_dropout_prob"          => 0.1,
   "intermediate_size"            => 3072,
 )
 
 #create bert model like pretrain struct
 #you can define the bert model in the way you like and wrap it with TransformerModel
-function create_bert()
+function create_albert()
   global config
-  bert = ALBert(
+  albert = ALBert(
     config["hidden_size"],
     config["num_attention_heads"],
     config["intermediate_size"],
     config["num_hidden_layers"];
     act = config["hidden_act"],
-    pdrop = config["hidden_dropout_prob"],
-    attn_pdrop = config["attention_probs_dropout_prob"]
+    embedding =config["embeddings"]
   )
 
-  tok_emb = Embed(
-    config["hidden_size"],
+  tok_emb1 = Embed(
+    config["embeddings"],
     config["vocab_size"]
+  )
+  tok_emb2 = Dense(
+    config["hidden_size"],
+    config["embeddings"]
   )
 
   seg_emb = Embed(
@@ -45,9 +47,6 @@ function create_bert()
   emb_post = Positionwise(
     LayerNorm(
       config["hidden_size"]
-    ),
-    Dropout(
-      config["hidden_dropout_prob"]
     )
   )
 
@@ -83,7 +82,9 @@ function create_bert()
   )
 
   emb = CompositeEmbedding(tok = tok_emb, pe = posi_emb, segment = seg_emb, postprocessor = emb_post)
+
+
   clf = (pooler = pooler, masklm = masklm, nextsentence = nextsentence)
 
-  TransformerModel(emb, bert, clf)
+  TransformerModel(emb, albert, clf)
 end
